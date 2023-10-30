f672fbba14c3c53785c351c5b30040afdadd9259
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-30 13:21:49 -0600

    phase 1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * 264
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs((destination - start)*264)
+}
+
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 401) {
+        return 0
+    } else if (distance > 400 && distance < 2001 ) {
+        return (distance-400) * .02
+    } else if (distance > 2000 && distance < 2501) {
+        return 25
+    } else if (distance > 2500) {
+        return "cannot travel that far"
+    }
+}
